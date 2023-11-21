9912c26d62bffb55cedd7050d713dc6b573af81e
jensenhill <91635059+jensenhill@users.noreply.github.com>
2023-11-21 09:31:45 +0000

    Create qr-scanner.js
    Moved JS Script from Index to external file.

---
+++ qr-scanner.js
@@ -0,0 +1,73 @@
+//console.log(jsQR); //jsQR library successfully loaded?
+let stream;
+let terminate = false;
+startCamera('environment'); //Atempt to start rear camera
+
+function startCamera(facingMode) {
+
+    navigator.mediaDevices.getUserMedia({video:{facingMode:{exact:facingMode}}}).then((streamResult) => { //Try opening camera stream for streamResult
+        stream = streamResult; //Save as stream
+        const videoStream = document.getElementById("camera"); //Get HTML <video> element and save as videoStream
+        videoStream.srcObject = stream;
+
+        videoStream.setAttribute('autoplay',''); //videoStream fix for Safari iOS
+        videoStream.setAttribute('playsinline',''); //videoStream fix for Safari iOS
+
+        videoStream.play(); //Video starts playing
+
+        videoStream.addEventListener("playing", () => { //Video play event... once video starts playing execute:
+            document.getElementById("output").innerText = "QR Code not detected"; //Update #output from 'awaiting camera' to 'no QR code found'
+            const canvas = document.createElement("canvas"); //Display video stream onto canvas
+            const renderingContext = canvas.getContext("2d"); //Rendering context as "2D"
+
+            const frameAnalysis = () => {
+                renderingContext.drawImage(videoStream, 0, 0, canvas.width, canvas.height); //Define height and width of frame + Draw frame to canvas.
+                const imageData = renderingContext.getImageData(0,0, canvas.width, canvas.height); //Image data for canvas packaged into imageData.
+                const code = jsQR(imageData.data, imageData.width, imageData.height); //jsQR function takes image data and returns object with QR info.
+
+                //QR Code Found
+                if (code) {
+                    console.log("The QR code was found: ", code.data); //Log data to console.
+                    
+                    //Inform user of success
+                    document.getElementById("output").innerText = code.data;
+                    document.getElementById("output").style.color = "green";
+                    stream.getTracks().forEach(track => track.stop()) //Terminate camera stream.
+                    videoStream.style.visibility = 'hidden'; //Once terminated, hide the container (otherwise the screen will be black).
+                    return;
+                }
+                //QR Code NOT Found (null object)
+                else {
+                    console.log("The QR code was not found."); //Log error to console
+                }
+
+                requestAnimationFrame(frameAnalysis)
+            };
+
+            frameAnalysis(); //Loop
+
+        })}).catch((error) => { //Catch error if rear 'environment' camera not available.
+            //Check if this error is because user denied permission 
+            if (error.name === 'NotAllowedError') {
+                console.log("User has denied access to the camera.");
+                document.getElementById("output").innerText = "Access to the camera has been denied."; //Inform user that we cannot access camera
+            } 
+            //User granted permission, but...
+            else {
+                //Try using a front facing camera.
+                if (facingMode === "environment") {
+                    startCamera("user"); //Call function using front camera this time.
+                }
+                //If both rear and front camera fail, attempt to let browser use device default camera.
+                else if (facingMode==="user")
+                {
+                    startCamera();
+                    terminate = true;
+                }
+                else if (terminate === true) 
+                {
+                    document.getElementById("output").innerText = "No camera was detected on this device."; //Inform user that we cannot access camera
+                }
+            }
+        })
+    }
